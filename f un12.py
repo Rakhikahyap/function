74ed2ae51f72b9ee38a5c8219b360a0f0e61d457
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-03-09 15:30:28 +0530

    Add files via upload

---
+++ f un12.py
@@ -0,0 +1,29 @@
+# def fun():
+#     a=[2],[2,3],[4],[6]
+#     i=0
+#     sum=0
+#     while i<len(a):
+#         j=0
+#         while j<len(a[i]):
+#             sum=sum+a[i][j]
+#             j=j+1
+#         i=i+1
+#     print(sum)
+# fun()
+
+# def fun(a,b):
+#     e=a+b
+#     return e
+# def fun1(d):
+#     g=fun(4,5)+d
+#     return g
+# print(fun1(5))
+
+# def sum(a,b):
+#     e=a+b
+#     print(e)
+#     def mul(a,b):
+#         f=a*b
+#         print(f)
+#     mul(4,3)
+# sum(4,2)
