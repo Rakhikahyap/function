d5c1830d034572ea0c99121ed3ed2149bc901c62
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-03-09 15:28:04 +0530

    Add files via upload

---
+++ f un6.py
@@ -0,0 +1,14 @@
+# def fun():
+#     num=int(input('enter a number'))
+#     i=1
+#     c=0
+#     while i<=num:
+#         if num%i==0:
+#             c=c+1
+#         i=i+1
+#     if c==2:
+#         print("prime number",num)
+#     else:
+#         print("no prime",num)
+# fun()
+
