a8cdc0bc816821f47533401e2eaa587515a34b54
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-03-09 16:58:36 +0530

    Add files via upload

---
+++ f un18.py
@@ -0,0 +1,19 @@
+# a=["Rishab","Rishab","abhishek","devil"]
+# i=0
+# n=[]
+# while i<len(a):
+#     if a[i] not in n:
+#         n.append(a[i])
+#     i=i+1
+# print(n)
+
+
+
+a=['a','a','b','c']
+i=0
+m=[]
+while i<len(a):
+    if a[i] not in m:
+        m.append(a[i])
+    i=i+1
+print(m)
