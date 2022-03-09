32727bc427a1627e7d22a6229399e087916848c9
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-03-09 15:29:11 +0530

    Add files via upload

---
+++ f un9.py
@@ -0,0 +1,29 @@
+
+# Recurtion
+# def sum(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return n+sum(n-1)
+# a=int(input("enter anumber"))
+# print(sum(a))
+
+
+# def fun(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return  n*fun(n-1)
+# n1=int(input("entre a num"))
+# b=fun(n1)
+# print(b)
+
+# def fun():
+#     c=4+3
+#     print(c)
+#     fun()
+# fun()
+
+def fun(a,b):
+    e=
+
